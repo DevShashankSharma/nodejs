11e7b3306b1dfcdb6e887b40482e4e933dc970d6
DevShashankSharma <editorshashank429@gmail.com>
2024-02-15 20:57:55 +0530

    Handling URL's in NodeJS

---
+++ 08_HandlingUrl/index.js
@@ -0,0 +1,46 @@
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+
+const port = 4000;
+
+const server = http.createServer((req, res) => {
+    if (req.url === "/favicon.ico") {
+        return res.end();
+    }
+    const log = `${Date.now()} ${req.method} ${req.url} : ${
+        req.headers["user-agent"]
+    }`;
+    const myUrl = url.parse(req.url, true);
+    console.log(myUrl);
+    fs.appendFile("./server.log", log + "\n", (err) => {
+        if (err) {
+        console.log(err);
+        }
+        console.log("log saved");
+        switch (myUrl.pathname) {
+        case "/":
+            res.end("Hello world");
+            break;
+        case "/about":
+            const userName = myUrl.query.name;
+            res.end(`About ${userName}`);
+            break;
+        case "/contact":
+            res.end("Contact");
+            break;
+        case "/search":
+            const search = myUrl.query.search_query;
+            res.end(`Search ${search}`);
+            break;
+        default:
+            res.end("Not Found page", 404);
+        }
+    });
+    // console.log('new server received',req.method, req.url);
+    // res.end('Hello world');
+    });
+
+    server.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+    });
