3ec04b62b4112048856887c61e5280ce56ae893c
DevShashankSharma <editorshashank429@gmail.com>
2024-02-21 06:52:26 +0530

    Introduction to POSTMAN for REST API's

--- 13_Building_Rest_api/index.js
+++ 13_Building_Rest_api/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
-const users = require("./MOCK_DATA.json")
+let users = require("./MOCK_DATA.json")
+const fs = require("fs");
 
 const app = express();
 const port = 4000;
+
+app.use(express.urlencoded({ extended: true }))
 
 app.get("/users", (req, res) => {
     const html = `
@@ -23,35 +26,64 @@ app.get("/api/users", (req, res) => {
 //     res.json(user)
 // })
 
-// app.post("/api/users", (req, res) => {
-//     // Todo create new user
-//     return res.send("Create new user pending")
-// })
+app.post("/api/users", (req, res) => {
+    let body = req.body
+    users.push({
+        id: users.length + 1,
+        ...body
+    })
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err) => {
+        if (err) {
+            return res.send({ status: "Failed to create new user" })
+        }
+        else {
+            return res.send({ status: "Successfully new user created" })
+        }
+    })
+})
 
-// app.patch("/api/users/:id", (req, res) => {
-//     // Todo update user with id 
-//     return res.send("Update user pending")
-// })
-
-// app.delete("/api/users/:id", (req, res) => {
-//     // Todo delete user with id 
-//     return res.send("Delete user pending")
-// })
-
-app.route("/api/users/:id").get((req, res) => {
+app.patch("/api/users/:id", (req, res) => {
     const id = Number(req.params.id)
     const user = users.find(user => user.id === id)
-    res.json(user)
-}).post((req, res) => {
-    // Todo create new user
-    return res.send({ status: "Create new user pending" })
-}).patch((req, res) => {
-    // Todo update user with id 
-    return res.send({ status: "Update user pending" })
-}).delete((req, res) => {
-    // Todo delete user with id 
-    return res.send({ status: "Delete user pending" })
+    user.first_name = req.body.first_name
+    user.last_name = req.body.last_name
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err) => {
+        if (err) {
+            return res.send({ status: "Failed to update user" })
+        }
+        else {
+            return res.send({ status: "Successfully user updated" })
+        }
+    })
 })
+
+app.delete("/api/users/:id", (req, res) => {
+    const id = Number(req.params.id)
+    users = users.filter(user => user.id !== id)
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err) => {
+        if (err) {
+            return res.send({ status: "Failed to delete user" })
+        }
+        else {
+            return res.send({ status: "Successfully user deleted" })
+        }
+    })
+})
+
+// app.route("/api/users/:id").get((req, res) => {
+//     const id = Number(req.params.id)
+//     const user = users.find(user => user.id === id)
+//     res.json(user)
+// }).post((req, res) => {
+//     // Todo create new user
+//     return res.send({ status: "Create new user pending" })
+// }).patch((req, res) => {
+//     // Todo update user with id 
+//     return res.send({ status: "Update user pending" })
+// }).delete((req, res) => {
+//     // Todo delete user with id 
+//     return res.send({ status: "Delete user pending" })
+// })
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
